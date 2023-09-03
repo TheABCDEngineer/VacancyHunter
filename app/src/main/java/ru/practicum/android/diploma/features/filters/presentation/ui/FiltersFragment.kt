072577686f25bb8c5299898c1f9cb8fea188bafd
InnaLapwa <inna27.02@yandex.ru>
2023-09-03 12:42:17 +0500

    Recycler для фильтра Отрасль

--- app/src/main/java/ru/practicum/android/diploma/features/filters/presentation/ui/FiltersFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/filters/presentation/ui/FiltersFragment.kt
@@ -12,6 +12,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.textfield.TextInputLayout
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
@@ -19,6 +20,7 @@ import ru.practicum.android.diploma.databinding.FragmentFiltersBinding
 import ru.practicum.android.diploma.features.filters.domain.models.Industry
 import ru.practicum.android.diploma.features.filters.presentation.models.FilterScreenState
 import ru.practicum.android.diploma.features.filters.presentation.viewModel.FiltersViewModel
+import java.util.Locale
 
 class FiltersFragment : Fragment() {
     private var _binding: FragmentFiltersBinding? = null
@@ -27,6 +29,8 @@ class FiltersFragment : Fragment() {
     private val viewModel by viewModel<FiltersViewModel>()
 
     private lateinit var salaryTextWatcher: TextWatcher
+    private lateinit var industrySearchTextWatcher: TextWatcher
+    private val industriesAdapter = IndustriesAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,6 +46,7 @@ class FiltersFragment : Fragment() {
 
         setMainScreenListeners()
         setIndustryScreenListeners()
+        customizeRecyclerView()
     }
 
     override fun onDestroy() {
@@ -79,11 +84,30 @@ class FiltersFragment : Fragment() {
     }
 
     private fun setIndustryScreenListeners() {
+        industrySearchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                industriesAdapter.industries.clear()
+                industriesAdapter.industries.addAll(viewModel.filterIndustries(s.toString()))
+                industriesAdapter.notifyDataSetChanged()
+            }
+        }
+        binding.filterIndustrySearchField.addTextChangedListener(industrySearchTextWatcher)
+
         binding.filterIndustryBack.setOnClickListener {
             render(FilterScreenState.MainScreen)
         }
     }
 
+    private fun customizeRecyclerView() {
+        binding.filterIndustriesRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.filterIndustriesRecyclerView.adapter = industriesAdapter
+    }
     private fun setEditTextColors(textInputLayout: TextInputLayout, text: CharSequence?) {
         val editTextColor = if (text.toString().isEmpty()) {
             ResourcesCompat.getColorStateList(resources, R.color.edit_text_color_selector, requireContext().theme)
@@ -114,6 +138,9 @@ class FiltersFragment : Fragment() {
         binding.filterMainLayout.visibility = View.GONE
         binding.filterIndustryLayout.visibility = View.VISIBLE
         binding.filterIndustrySearchField.text.clear()
+
+        industriesAdapter.industries = viewModel.getIndustries()
+        industriesAdapter.notifyDataSetChanged()
     }
 
     private fun showWorkPlace() {
---
+++ app/src/main/java/ru/practicum/android/diploma/features/filters/presentation/ui/IndustriesViewHolder.kt
@@ -0,0 +1,15 @@
+package ru.practicum.android.diploma.features.filters.presentation.ui
+
+import android.view.View
+import android.widget.RadioButton
+import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.features.filters.domain.models.Industry
+
+class IndustriesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    private val industryRB: RadioButton = itemView.findViewById(R.id.filter_industry_radioButton)
+
+    fun bind(industry: Industry) {
+        industryRB.setText(industry.name)
+    }
+}
