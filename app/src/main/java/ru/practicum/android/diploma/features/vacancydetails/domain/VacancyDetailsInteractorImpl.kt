bb2d16e35286082ebf414f769be1b93f8c3fed20
pogonat <aimagenta81@gmail.com>
2023-09-12 18:39:33 +0600

    GH-86: refactor getVacancyById in VacancyDetailsInteractorImpl

--- app/src/main/java/ru/practicum/android/diploma/features/vacancydetails/domain/VacancyDetailsInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/vacancydetails/domain/VacancyDetailsInteractorImpl.kt
@@ -14,23 +14,42 @@ class VacancyDetailsInteractorImpl(
 ) : VacancyDetailsInteractor {
 
     override suspend fun getVacancyById(id: String): Outcome<VacancyDetails> {
-        val outcome = vacancyRepository.getVacancyById(id)
+        val outcome = searchVacancyById(id)
+
+        val isFavorite = checkIfIsFavorite(id)
+        if (!isFavorite) return outcome
 
         when (outcome) {
-            is Outcome.Error -> return outcome
+            is Outcome.Error -> {
+                return fetchFromFavoritesDb(id)
+            }
+
             is Outcome.Success -> {
-                val vacancyDetails = outcome.data ?: return outcome
-                val isFavorite = withContext(Dispatchers.IO) {
-                    favoritesRepository.checkIfIsFavorite(id)
-                }
-                return if (isFavorite) {
-                    val vacancyMarkedFavorite = vacancyDetails.copy(isFavorite = true)
-                    Outcome.Success(vacancyMarkedFavorite)
-                } else {
-                    outcome
-                }
+                val vacancyDetails = outcome.data
+                if (vacancyDetails == null) return fetchFromFavoritesDb(id)
+                val vacancyMarkedFavorite = vacancyDetails.copy(isFavorite = true)
+                return Outcome.Success(vacancyMarkedFavorite)
             }
         }
+    }
+
+    private suspend fun searchVacancyById(id: String): Outcome<VacancyDetails> {
+        return withContext(Dispatchers.IO) {
+            vacancyRepository.getVacancyById(id)
+        }
+    }
+
+    private suspend fun checkIfIsFavorite(id: String): Boolean {
+        return withContext(Dispatchers.IO) {
+            favoritesRepository.checkIfIsFavorite(id)
+        }
+    }
+
+    private suspend fun fetchFromFavoritesDb(id: String): Outcome<VacancyDetails> {
+        val vacancyFromFavorites = withContext(Dispatchers.IO) {
+            favoritesRepository.getVacancyById(id)
+        }
+        return Outcome.Success(data = vacancyFromFavorites)
     }
 
 }
