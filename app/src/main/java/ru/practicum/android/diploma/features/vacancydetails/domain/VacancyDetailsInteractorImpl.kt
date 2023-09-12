c77508ac2b02b5f89df5a9fa3b4ec672bbfe221d
pogonat <aimagenta81@gmail.com>
2023-09-12 14:29:31 +0600

    GH-86: add marking vacancy as favorite to VacancyDetails interactor

--- app/src/main/java/ru/practicum/android/diploma/features/vacancydetails/domain/VacancyDetailsInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/vacancydetails/domain/VacancyDetailsInteractorImpl.kt
@@ -1,16 +1,32 @@
 package ru.practicum.android.diploma.features.vacancydetails.domain
 
+import ru.practicum.android.diploma.features.favorites.domain.FavoritesRepository
 import ru.practicum.android.diploma.features.vacancydetails.domain.models.VacancyDetails
 import ru.practicum.android.diploma.root.domain.VacancyRepository
 import ru.practicum.android.diploma.root.domain.model.Outcome
 
 
 class VacancyDetailsInteractorImpl(
-    private val vacancyRepository: VacancyRepository
+    private val vacancyRepository: VacancyRepository,
+    private val favoritesRepository: FavoritesRepository
 ) : VacancyDetailsInteractor {
 
     override suspend fun getVacancyById(id: String): Outcome<VacancyDetails> {
-        return vacancyRepository.getVacancyById(id)
+        val outcome = vacancyRepository.getVacancyById(id)
+
+        when (outcome) {
+            is Outcome.Error -> return outcome
+            is Outcome.Success -> {
+                val vacancyDetails = outcome.data ?: return outcome
+                val isFavorite = favoritesRepository.checkIfIsFavorite(id)
+                return if (isFavorite) {
+                    val vacancyMarkedFavorite = vacancyDetails.copy(isFavorite = true)
+                    Outcome.Success(vacancyMarkedFavorite)
+                } else {
+                    outcome
+                }
+            }
+        }
     }
 
 }
